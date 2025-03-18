8337e1da9bd763d7f9f939e349aff9dbbca719d7
24Yashwanth24 <kyashwanth831@gmail.com>
2025-03-18 11:56:36 +0530

    made add compoents page and applied test routings to all

--- voltmanager/lib/main.dart
+++ voltmanager/lib/main.dart
@@ -99,10 +99,163 @@ class AddComp extends StatefulWidget {
 }
 
 class AddCompState extends State<AddComp> {
+  final _formKey = GlobalKey<FormState>();
+  final rangeController = TextEditingController();
+  final valueController = TextEditingController();
+  final quantityController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Form(child: Column(children: [
-             
-        ],)));
+    return Center(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            DropdownButtonFormField<String>(
+              decoration: InputDecoration(labelText: 'Select Component'),
+              items:
+                  [
+                    'Resistor',
+                    'Capacitor',
+                    'Inductor',
+                    'Diode',
+                    'Transistor',
+                    'IC',
+                    'LED',
+                    'PCB',
+                    'Battery',
+                    'Others',
+                  ].map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+              onChanged: (value) {
+                // Handle value change
+              },
+              validator: (value) {
+                if (value == null) {
+                  return 'Please select a component';
+                }
+                return null;
+              },
+            ),
+            DropdownButtonFormField<String>(
+              decoration: InputDecoration(labelText: 'Component Type'),
+              items:
+                  ['SMD', 'Through-Hole', 'Others'].map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+              onChanged: (value) {
+                // Handle value change
+              },
+              validator: (value) {
+                if (value == null) {
+                  return 'Please declare component Type';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Range',
+                hintText: 'Enter component Range',
+              ),
+              validator: (value) {
+                return null;
+              },
+              controller: rangeController,
+            ),
+            Row(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Value',
+                    hintText: 'Component Value',
+                  ),
+                  validator: (value) {
+                    return null;
+                  },
+                  keyboardType: TextInputType.number, // Numeric keyboard
+                  controller: valueController,
+                ),
+                DropdownButtonFormField<String>(
+                  items:
+                      ['k', 'm', 'u', 'n', 'M'].map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                  onChanged: (value) {},
+                ),
+              ],
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Quantity',
+                hintText: 'Component Quantity',
+              ),
+              validator: (value) {
+                if (value == null) {
+                  return 'Please enter component Quantity';
+                }
+                return null;
+              },
+              keyboardType: TextInputType.number, // Numeric keyboard
+              controller: quantityController,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  // Save data to database
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        'Components added to database successfully!!',
+                      ),
+                    ),
+                  );
+                  // reset the form
+                  _formKey.currentState!.reset();
+                  rangeController.clear();
+                  valueController.clear();
+                  quantityController.clear();
+                }
+              },
+              child: Text("Add"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ViewComp extends StatelessWidget {
+  const ViewComp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Sorry, this page is not developed yet'));
+  }
+}
+
+class UseComp extends StatelessWidget {
+  const UseComp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Sorry, this page is not developed yet'));
+  }
+}
+
+class CalcComp extends StatelessWidget {
+  const CalcComp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Sorry, this page is not developed yet'));
   }
 }
