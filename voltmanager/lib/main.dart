d971a8fa21aa4936cd5f0003e9cfc6f6f2825c91
24Yashwanth24 <kyashwanth831@gmail.com>
2025-03-18 10:56:43 +0530

    main.dart update 1

--- voltmanager/lib/main.dart
+++ voltmanager/lib/main.dart
@@ -6,6 +6,103 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(body: Center()));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => AddComp()),
+                          );
+                        },
+                        icon: Icon(Icons.add),
+                        iconSize: 80,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => ViewComp()),
+                          );
+                        },
+                        icon: Icon(Icons.visibility),
+                        iconSize: 80,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => UseComp()),
+                          );
+                        },
+                        icon: Icon(Icons.shopping_cart),
+                        iconSize: 80,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => CalcComp()),
+                          );
+                        },
+                        icon: Icon(Icons.calculate_sharp),
+                        iconSize: 80,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AddComp extends StatefulWidget {
+  const AddComp({super.key});
+
+  @override
+  State<AddComp> createState() => AddCompState();
+}
+
+class AddCompState extends State<AddComp> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Form(child: Column(children: [
+             
+        ],)));
   }
 }
